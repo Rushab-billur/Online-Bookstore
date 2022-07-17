af91a058c6d30165ebed2552b4a971a1a208c321
Rushab-billur <109464229+Rushab-billur@users.noreply.github.com>
2022-07-17 17:05:37 +0530

    Add files via upload

---
+++ template/footer.php
@@ -0,0 +1,12 @@
+      	<hr>
+
+      	<footer>
+        	<div class="text-muted pull-right" style="color: white">
+          		<a style="color: skyblue" href="admin.php">Admin Login</a> 2021
+        	</div>
+      	</footer>
+    </div> <!-- /container -->
+    <script type="text/javascript" src="./bootstrap/js/jquery-2.1.4.min.js"></script>
+    <script type="text/javascript" src="./bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
